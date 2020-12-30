0ef1bdfd4a5eddf6007265882cfbdb60399ef018
Nathan Keller <keller.nathan@gmail.com>
2020-12-30 15:44:44 -0700

    add test for no_api_key

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -7,13 +7,21 @@ class ApiTest < Minitest::Test
   def test_no_api_key
     make_request('?s=star', 'http://www.omdbapi.com/')
     puts last_response.body
-    # TODO: Add the assertion described in readme
+    parse_last_response_body = JSON.parse(last_response.body)
+
+    # expect a JSON response
+    assert_equal last_response.headers["Content-Type"], "application/json; charset=utf-8"
+    # expect correct error status code
+    assert_equal last_response.status, 401
+    assert_equal parse_last_response_body.key?('Response'), true
+    assert_equal parse_last_response_body.key?('Error'), true
+    assert_equal parse_last_response_body['Error'], 'No API key provided.'
   end
 
   def test_titles_are_relevant_match
+    skip
     make_request("?apikey=#{ENV['OMDB_API_KEY']}&s=star", 'http://www.omdbapi.com/')
     puts last_response.body
-
 
   end
 end
