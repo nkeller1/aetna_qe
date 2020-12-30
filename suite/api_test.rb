6dcc9ff97e855163c55d96c7d1d8f5fa90e19a8a
Nathan Keller <keller.nathan@gmail.com>
2020-12-30 16:51:48 -0700

    Test a successful response

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -5,12 +5,14 @@ require 'minitest/autorun'
 class ApiTest < Minitest::Test
 
   def test_no_api_key
+    skip
     make_request('?s=star', 'http://www.omdbapi.com/')
     puts last_response.body
     parse_last_response_body = JSON.parse(last_response.body)
 
-    # expect a JSON response
+    # expect response to be JSON formatted
     assert_equal last_response.headers["Content-Type"], "application/json; charset=utf-8"
+    assert_instance_of Hash, parse_last_response_body
     # expect correct error status code
     assert_equal last_response.status, 401
     assert_equal parse_last_response_body.key?('Response'), true
@@ -18,10 +20,31 @@ class ApiTest < Minitest::Test
     assert_equal parse_last_response_body['Error'], 'No API key provided.'
   end
 
-  def test_titles_are_relevant_match
-    skip
-    make_request("?apikey=#{ENV['OMDB_API_KEY']}&s=star", 'http://www.omdbapi.com/')
-    puts last_response.body
+  def test_successful_response_for_thomas
+    make_request("?apikey=#{ENV['OMDB_API_KEY']}&s=thomas", 'http://www.omdbapi.com/')
+    parse_last_response_body = JSON.parse(last_response.body)
+    search_results =  parse_last_response_body['Search']
+
+    assert_equal last_response.status, 200
+    assert_equal last_response.headers["Content-Type"], "application/json; charset=utf-8"
+    assert_instance_of Hash, parse_last_response_body
+    assert_instance_of Array, search_results
+    assert_equal parse_last_response_body.key?('Search'), true
+    search_results.each do |result|
+      # Verify all titles are a relevant match
+      assert_includes result['Title'], 'Thomas' || 'thomas'
+      # Verify keys include Title, Year, imdbID, Type, and Poster for all records in the response
+      assert_equal result.key?('Title'), true
+      assert_equal result.key?('Year'), true
+      assert_equal result.key?('imdbID'), true
+      assert_equal result.key?('Type'), true
+      assert_equal result.key?('Poster'), true
+      assert_instance_of Hash, result
+      # Expect 'Year' Key to be formatted correctly
+      assert_equal Date.new(result['Year'].to_i).gregorian?, true
+      #expect poster to be a URL
+      assert_includes result['Poster'], 'http'
+    end
 
   end
 end
