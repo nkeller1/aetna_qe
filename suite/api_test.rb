982161cde5a2982d54be9109e9a4f0e541ecf1a5
Nathan Keller <keller.nathan@gmail.com>
2021-01-16 15:49:02 -0700

    Adjust a few tests based on feedback

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -9,31 +9,28 @@ class ApiTest < Minitest::Test
     # puts last_response.body
     parse_last_response_body = JSON.parse(last_response.body)
 
-    # expect response to be JSON formatted
-    assert_equal last_response.headers["Content-Type"], "application/json; charset=utf-8"
-    assert_instance_of Hash, parse_last_response_body
-    # expect correct error status code
-    assert_equal last_response.status, 401
-    assert_equal parse_last_response_body.key?('Response'), true
     assert_equal parse_last_response_body['Response'], 'False'
-    assert_equal parse_last_response_body.key?('Error'), true
     assert_equal parse_last_response_body['Error'], 'No API key provided.'
+    refute_equal last_response.status, 200
   end
 
   def test_successful_response_for_search_of_thomas
     make_request("?apikey=#{ENV['OMDB_API_KEY']}&s=thomas", 'http://www.omdbapi.com/')
     # puts last_response.body
-    parse_last_response_body = JSON.parse(last_response.body)
+    begin
+      parse_last_response_body = JSON.parse(last_response.body)
+      # the following line is to check that the error is being properly handled.
+      # parse_last_response_body = JSON.parse('last_response.body')
+    rescue JSON::ParserError
+      puts 'JSON Parser Error, check that last_response is a JSON object'
+    end
+
     search_results =  parse_last_response_body['Search']
 
-    assert_equal last_response.status, 200
-    assert_equal last_response.headers["Content-Type"], "application/json; charset=utf-8"
-    assert_instance_of Hash, parse_last_response_body
-    assert_instance_of Array, search_results
     assert_equal parse_last_response_body.key?('Search'), true
     search_results.each do |result|
       # Verify all titles are a relevant match
-      assert_includes result['Title'], 'Thomas' || 'thomas'
+      assert_includes result['Title'].downcase, 'thomas'
       # Verify keys include Title, Year, imdbID, Type, and Poster for all records in the response
       assert_equal result.key?('Title'), true
       assert_equal result.key?('Year'), true
@@ -64,28 +61,18 @@ class ApiTest < Minitest::Test
   end
 
   def test_poster_link_validity_page_1
-    make_request("?apikey=#{ENV['OMDB_API_KEY']}&s=thomas&page=1", 'http://www.omdbapi.com/')
-    # puts last_response.body
-    parse_last_response_body = JSON.parse(last_response.body)
-    search_results =  parse_last_response_body['Search']
-
-    search_results.each do |result|
-      assert_instance_of (URI::HTTPS || URI::HTTP), URI.parse(result['Poster'])
-    end
-  end
-
-  def test_invalid_poster_links_are_okay_on_page_3
-    # additonal edge case for invalid poster links
     make_request("?apikey=#{ENV['OMDB_API_KEY']}&s=thomas&page=3", 'http://www.omdbapi.com/')
     # puts last_response.body
     parse_last_response_body = JSON.parse(last_response.body)
     search_results =  parse_last_response_body['Search']
-
     search_results.each do |result|
-      if !URI.parse(result['Poster']).is_a?(URI::HTTPS)
-        assert_equal result['Poster'], "N/A"
+      poster_link = result['Poster']
+      begin
+        response = make_request(poster_link)
+      rescue
+        assert_equal poster_link, 'N/A'
       else
-        assert_instance_of (URI::HTTPS || URI::HTTP), URI.parse(result['Poster'])
+        assert_equal response.status, 200
       end
     end
   end
@@ -112,27 +99,21 @@ class ApiTest < Minitest::Test
   end
 
 # additional testing I am curious about
-  def test_imbdID_is_uniform_for_all_responses_page_1
+  def test_imbdID_is_uniform_for_all_requests_on_page_1
     make_request("?apikey=#{ENV['OMDB_API_KEY']}&s=thomas&page=1", 'http://www.omdbapi.com/')
     # puts last_response.body
     parse_last_response_body = JSON.parse(last_response.body)
     search_results =  parse_last_response_body['Search']
+
+    def is_numeric?(numeric_id)
+      numeric_id.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+    end
+
     search_results.each do |result|
-      verify_id = result['imdbID'].split(//)
-      assert_equal verify_id.length, 9
-      assert_equal verify_id[0], 't'
-      assert_equal verify_id[1], 't'
-      # verifies that the trailing int's are actual int's
-      assert_equal verify_id[2].to_i.to_s, verify_id[2]
-      assert_equal verify_id[3].to_i.to_s, verify_id[3]
-      assert_equal verify_id[4].to_i.to_s, verify_id[4]
-      assert_equal verify_id[5].to_i.to_s, verify_id[5]
-      assert_equal verify_id[6].to_i.to_s, verify_id[6]
-      assert_equal verify_id[7].to_i.to_s, verify_id[7]
-      assert_equal verify_id[8].to_i.to_s, verify_id[8]
-      # verifies leading t's, that convert to INT 0, are not equal
-      refute_match verify_id[0].to_i.to_s, verify_id[0]
-      refute_match verify_id[1].to_i.to_s, verify_id[1]
+      imdbID = result['imdbID']
+      assert_equal imdbID.length, 9
+      assert_equal imdbID[0..1], 'tt'
+      assert_equal is_numeric?(imdbID[2..8]), true
     end
   end
 
